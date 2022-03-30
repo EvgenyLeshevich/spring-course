2a1fc3b1071af8b854b977de5e70cb40ce376572
Evgeniy Leshevich <evgeniy.le99@gmail.com>
2022-03-30 17:26:06 +0300

    Create simple spring-rest project

---
+++ src/main/java/com/evgeniy/controller/MainController.java
@@ -0,0 +1,65 @@
+package com.evgeniy.controller;
+
+import com.evgeniy.entity.Employee;
+import com.evgeniy.exception_handling.NoSuchEmployeeException;
+import com.evgeniy.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MainController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable("id") Long id) {
+
+        Employee employee = employeeService.getEmployee(id);
+
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " + id + " in Database");
+        }
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable("id") Long id) {
+
+        Employee employee = employeeService.getEmployee(id);
+
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " + id + " in Database");
+        }
+
+        employeeService.deleteEmployee(id);
+
+        return "Employee with ID = " + id + " was deleted";
+    }
+}
---
+++ src/main/java/com/evgeniy/exception_handling/EmployeeIncorrectData.java
@@ -0,0 +1,13 @@
+package com.evgeniy.exception_handling;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class EmployeeIncorrectData {
+
+    private String info;
+}
